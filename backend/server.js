8a509a247da24658f19c33f6e49773e434926292
SadraCop <hfsabzevari@gmail.com>
2025-07-10 00:04:07 +0330

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
-import path from "path";
+import pathToRegexp from "pathToRegexp";
 import todoRoutes from "./routes/todo.route.js";
 const PORT = process.env.PORT || 5000;
 
@@ -15,12 +15,12 @@ app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
 
-const __dirname = path.resolve();
+const __dirname = pathToRegexp.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(pathToRegexp.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(pathToRegexp.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
